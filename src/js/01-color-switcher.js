fdf762699becec09a993bd4ef24f9bf8e81c4c88
vladakalyta <vladakalyta@gmail.com>
2022-08-28 17:37:33 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+startButton.addEventListener('click', changeBgColor);
+stopButton.addEventListener('click', stopChangeBgColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+function changeBgColor() {
+  timerId = setInterval(() => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', 'disabled');
+  stopButton.removeAttribute('disabled');
+}
+
+function stopChangeBgColor() {
+  clearInterval(timerId);
+  stopButton.setAttribute('disabled', 'disabled');
+  startButton.removeAttribute('disabled');
+}
